f67ae0471d084f12d6d136776d71c90d5273a442
James Montebell <100632392+jamesmontebell@users.noreply.github.com>
2023-09-29 18:44:30 -0400

    project1-1

---
+++ AdapterPattern.md
@@ -0,0 +1,8 @@
+$ Adapter Pattern
+
+$$ Two kinds of adapter pattern
+
+$$$ Object adapter pattern
+- composition design
+- create an object of another class and use it's methods.
+
---
+++ project1-1/project1-1/src/main/java/org/example/Ship.java
@@ -0,0 +1,16 @@
+package org.example;
+
+public class Ship {
+    private int length;
+    private String name;
+    Ship(int length, String name)
+    {
+        this.length = length;
+        this. name = name;
+    }
+
+    public int getLength()
+    {
+        return length;
+    }
+}
